85f3b9f0c802d6226ccf678e425c52d52b5fdca0
Robin Ricard <ricard.robin@gmail.com>
2014-04-28 20:48:35 +0200

    New boot !

--- boot.sh
+++ boot.sh
@@ -3,7 +3,7 @@ REPO_DIR=~/src
 CM_REMOTE=github.com/osxc/xc-common
 CM_REPO=$REPO_DIR/$RL_REMOTE
 
-CS_REMOTE=$1
+CS_REMOTE=$0
 CS_REPO=$REPO_DIR/$CS_REMOTE
 
 echo "osxc bootstrap script"
@@ -18,26 +18,44 @@ echo " * Custom Installation Path: $CS_REPO"
 echo ""
 echo ""
 
-echo "Ansible installation"
-echo "--------------------"
-sudo easy_install pip
-sudo pip install ansible
-echo ""
-echo ""
+if [ -f "/Library/Developer/CommandLineTools/usr/bin/clang" ]; then
+  echo "XCode Tools Installation"
+  echo "------------------------"
+  echo ""
+  echo "This will open up a modal window ... Get back here when ready !"
+  sudo /usr/bin/xcode-select --install
+  read -p "Continue ? [Enter]"
+  echo ""
+  echo ""
+fi
 
-echo "Clone with tarball"
+if [ -f "/usr/local/bin/ansible" ]; then
+  echo "Ansible installation"
+  echo "--------------------"
+  export CFLAGS=-Qunused-arguments
+  export CPPFLAGS=-Qunused-arguments
+  sudo -E easy_install pip
+  sudo -E pip install ansible
+  echo ""
+  echo ""
+fi
+
+echo "Common tools clone"
 echo "------------------"
-mkdir -p $CM_REPO
-mkdir -p $CS_REPO
-
-curl -L "https://$CM_REMOTE/tarball/master" | tar --strip-components=1 -zx -C $CM_REPO
-curl -L "https://$CS_REMOTE/tarball/master" | tar --strip-components=1 -zx -C $CS_REPO
+git clone https://$CM_REMOTE.git $CM_REPO
 echo ""
 echo ""
 
-echo "First Ansible Run"
-echo "-----------------"
+echo "Custom tools clone"
+echo "------------------"
+git clone https://$CS_REMOTE.git $CS_REPO
 ln -s $CM_REPO/roles/common $CS_REPO/roles/common
-ansible-playbook -i $CM_REPO/hosts $CS_REPO/all.yml
 echo ""
 echo ""
+
+echo "Congratulations ! "
+echo "----------------- "
+echo "You bootstrapped osxc ! "
+echo "Now it's time to launch some osxc configuration with: "
+echo ""
+echo "    TODO: real CLI tools :) ! "

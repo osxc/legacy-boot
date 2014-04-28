d1e0d88c44f8364d0920f7735fa371caa03f781d
Robin Ricard <ricard.robin@gmail.com>
2014-04-28 21:16:11 +0200

    Defaults

--- boot.sh
+++ boot.sh
@@ -3,7 +3,7 @@ REPO_DIR=~/src
 CM_REMOTE=github.com/osxc/xc-common
 CM_REPO=$REPO_DIR/$CM_REMOTE
 
-CS_REMOTE=$1
+CS_REMOTE=${1:-github.com/osxc/xc-custom}
 CS_REPO=$REPO_DIR/$CS_REMOTE
 
 echo "osxc bootstrap script"
